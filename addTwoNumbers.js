038a906bc3f7662d14043dd1e718f405acceccf5
MarcoDayz <diaz.marco8686@gmail.com>
2022-11-13 12:58:53 -0800

    added new algos

---
+++ addTwoNumbers.js
@@ -0,0 +1,20 @@
+/**
+Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+link1 = [2,4,3]
+link2 = [5,6,4]
+
+link01 = [0]
+link02 = [0]
+
+link001 = [9,9,9,9,9,9,9]
+link002 = [9,9,9,9]
+**/
+
+var addTwoNumbers = function(l1, l2) {
+    
+}
--- twoSum2.js
+++ twoSum2.js
@@ -5,19 +5,14 @@
 let arr = [2,7,14,15]
 let target = 17
 
-const twoSum = function(arr, target) {
+const twoSum = (arr, target) => {
 let container = {};
-
 for(let i = 0; i <= arr.length; i++){
     const current = arr[i];
-
     const needNum = target - current;
     const index = container[needNum];
-    console.log(index)
     if(index != null){
-
         return [index, i]
-        
     }else{
         container[current] = i
     }
